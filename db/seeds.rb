170152069c3bec4415427489fa478b24b68e7a15
Nguyen Toan <toannq@nal.vn>
2015-05-08 00:22:09 +0700

    Sua loi

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,53 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Book.create!(name:                "Dế mèn phiêu lưu ký",
+             url:                 "http://vnaudiobook.com/Upload/Book/Avatar/De-men-phieu-luu-kyCGSIE.jpg",
+             price:               "18.7",
+             author_id:           "1",
+             categories_id:       "1")
+
+Book.create!(name:                "Cát bụi chân ai",
+             url:                 "http://farm6.staticflickr.com/5476/12603504095_7a1df1b94c_o.jpg",
+             price:               "20.3",
+             author_id:           "1",
+             categories_id:       "1")
+
+Author.create!(name:              "Tô Hoài",
+               email:             "tohoai@yahoo.com")
+
+Author.create!(name:              "Nguyễn Du",
+               email:             "nguyendu@yahoo.com")
+
+Author.create!(name:              "Nguyễn Trãi",
+               email:             "nguyentrai@yahoo.com")
+
+Author.create!(name:              "Đỗ Phủ",
+               email:             "dophu@yahoo.com")
+
+Author.create!(name:              "Lý Bach",
+               email:             "lybach@yahoo.com")
+
+Category.create!(name:            "Lập trình máy tính")
+
+Category.create!(name:            "Đồ hoạ máy tính")
+
+Category.create!(name:            "Ngoại ngữ")
+
+Category.create!(name:            "Kinh tế - xã hội")
+
+Category.create!(name:            "Văn học Việt Nam")
+
+10.times do |n|
+  name = Faker::Name.title
+  url = Faker::Avatar.image
+  price = Faker::Bitcoin.rand_in_range(20, 50)
+  author_id = "1"
+  categories_id = "1"
+  Book.create!(name:                name,
+               url:                 url,
+               price:               price,
+               author_id:           author_id,
+               categories_id:       categories_id)
+end
